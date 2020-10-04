56a10d93a334d10fdb8679e89a3bb95a731477fc
Ed Himwich <51408905+wehimwich@users.noreply.github.com>
2020-10-04 16:15:10 +0000

    DBBC3 Tsys display
    Removed dropping decimal in dbl2str and flt2str: doesn't round properly

--- clib/str_util.c
+++ clib/str_util.c
@@ -99,15 +99,13 @@ int deci;         /* digits after decimal point, >=0 blank fill for right   */
 	}
       }
 
-      if(strlen(string)>wide && string[wide-1]!='.' && string[wide]!='.' || !isnum) {     /* too wide or nan/inf, $ fill */
+      if(strlen(string)>wide || !isnum) {     /* too wide or nan/inf, $ fill */
         for (i=0; i< wide; i++) 
           output[i]='$';
         output[wide]='\0';
         return;
-      } else {
-        string[wide]=0;
+      } else
         strcat(output,string);      /* okay, append result */
-      }
 
        return;
 }
@@ -164,15 +162,13 @@ int deci;         /* digits after decimal point, >=0 blank fill for right   */
 	}
       }
 
-      if(strlen(string)>wide && string[wide-1]!='.' && string[wide]!='.' || !isnum) {     /* too wide or nan/inf, $ fill */
+      if(strlen(string)>wide || !isnum) {     /* too wide or nan/inf, $ fill */
         for (i=0; i< wide; i++) 
           output[i]='$';
         output[wide]='\0';
         return;
-      } else {
-        string[wide]=0;
+      } else
         strcat(output,string);      /* okay, append result */
-      }
 
        return;
 }
